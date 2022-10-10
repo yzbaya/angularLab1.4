22dc3bc17173fe6d315458238b30e85e3468805a
yzbaya <101043195+yzbaya@users.noreply.github.com>
2022-10-10 10:49:53 +0100

    lab2car

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { Car } from './car';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+template: `
+
+<table>
+  <tr *ngFor = "let car of _cars">
+    <td>{{car.make}}</td>
+    <td>{{car.model}}</td>
+    <td><button (click)="showCar(car)">Click me !</button></td>
+    <!-- <td><button (click)="showCar(car)">showPHOTO !</button></td> -->
+  </tr>
+<table>
+
+`,
+styles: []
 })
-export class AppComponent {
-  title = 'angular2';
+
+
+export class AppComponent{
+_cars = [new Car('car1', 2002, 'bmw', 'm3', 'https://en.wikipedia.org/wiki/BMW_M3'),
+
+  new Car('car2', 2017, 'acura', 'nsx','https://en.wikipedia.org/wiki/Honda_NSX'),
+  new Car('car3', 2016, 'chevy', 'camaro', 'https://en.wikipedia.org/wiki/Chevrolet_Camaro')];
+
+   showCar(Car: any){
+    const desc = Car.make;
+    if (window.confirm('click "ok" to be redirected to ' + Car.article)){
+      window.location=Car.article;
+      }
+
+    }
+        
 }
